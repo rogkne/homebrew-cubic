f9b1a9c38baeb0cd0776e6681c6416b0c815122f
Roger Knecht <rknecht@pm.me>
2026-05-10 16:35:37 +0000

    chore: update to v0.22.1
    Signed-off-by: Roger Knecht <rknecht@pm.me>

--- cubic.rb
+++ cubic.rb
@@ -1,12 +1,11 @@
 class Cubic < Formula
   desc "Lightweight command-line manager for virtual machines"
   homepage "https://github.com/cubic-vm/cubic"
-  url "https://github.com/cubic-vm/cubic/archive/refs/tags/v0.21.0.tar.gz"
-  sha256 "184317a8db18cc7da35266ba4f81ede390c4c52f167c2a4578c40a374d6111b1"
+  url "https://github.com/cubic-vm/cubic/archive/refs/tags/v0.22.1.tar.gz"
+  sha256 "7c8a4331a2530b374d83ae627e0d2a3c4630b17627b9ed164f671582b6a6aa67"
   license "MIT OR Apache-2.0"
 
   depends_on "rust" => :build
-  depends_on "cdrtools"
   depends_on "qemu"
 
   def install
@@ -15,6 +14,6 @@ class Cubic < Formula
   end
 
   test do
-    system bin/"cubic", "ls"
+    system bin/"cubic", "instances"
   end
 end
